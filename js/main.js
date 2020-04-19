c75152a22e07f3c783ad80a969d3a2973bca0fda
senei <senei@senei-web.local>
2020-04-19 10:25:45 +0200

    font face observer added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 import { SVG } from "/js/svg.esm.js";
-//console.info(SVG);
+import FontFaceObserver from "/web_modules/fontfaceobserver/fontfaceobserver.js";
+
 var draw = SVG().addTo("#drawing").size(640,480);
-    // draw.rect(100, 100).attr({ fill: "#f06" });
-    draw.text("senei").attr("class", "main").attr("x", 210).attr("y", 155);
-    draw.text("web").attr("class", "sub").attr("x", 347).attr("y", 213);
+var StencilRoughLisener = new FontFaceObserver("StencilRough");
+
+StencilRoughLisener.load().then(function () {
+  draw.text("senei").attr("class", "main").attr("x", 210).attr("y", 155);
+  draw.text("web").attr("class", "sub").attr("x", 347).attr("y", 213);
+});
