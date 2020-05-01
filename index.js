043b38260125f36965f7f6e11c944c8c361fc193
senei <senei@senei-web.local>
2020-05-01 22:02:05 +0200

    vite - remastering

---
+++ index.js
@@ -0,0 +1,9 @@
+import { withScopeId as e, pushScopeId as a, createTextVNode as t, createVNode as n, popScopeId as s, openBlock as o, createBlock as r, defineComponent as c, createApp as u,
+} from "https://unpkg.com/vue@3.0.0-beta.5/dist/vue.esm-browser.prod.js";
+var d = {};
+const m = e("data-v-a69668ba");
+a("data-v-a69668ba");
+const b = { class: "flex-auto text-center name" }, l = t("senei "), v = n("small", null, "web", -1);
+s();
+const i = m(function (e, a) { return o(), r("h1", b, [l, v]); });
+(d.render = i), (d.__scopeId = "data-v-a69668ba"), u(c(d)).mount("#senei");
