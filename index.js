ad35c0b61c59383390b14693d513a7e3ccdc2dc4
senei <senei@senei-web.local>
2020-05-05 18:25:55 +0200

    web_modules / vue

--- index.js
+++ index.js
@@ -1,1 +1,134 @@
-import{ref as s,computed as e,withScopeId as n,pushScopeId as t,createVNode as a,popScopeId as c,openBlock as r,createBlock as o,defineComponent as l,createApp as i}from"https://unpkg.com/vue@3.0.0-beta.9/dist/vue.esm-browser.prod.js";var g={setup(){const n=s("start"),t=e(()=>"flex-auto text-center name title-transform--"+n.value);return{transformId:n,transformClass:t,trigger:function(s,e){n.value=s}}}};const u=n("data-v-ca4cb224");t("data-v-ca4cb224");const m={class:"flex mx-auto mt-5 mb-5 logo"},d=a("span",{class:"_w"},"w",-1),p=a("span",{class:"_e"},"e",-1),w=a("span",{class:"_b"},"b",-1);c();const b=u((function(s,e){return r(),o("section",m,[a("h1",{class:s.transformClass},[a("span",{class:"s",onClick:e[1]||(e[1]=e=>s.trigger("s","who"))},"s"),a("span",{class:"e1",onClick:e[2]||(e[2]=e=>s.trigger("e1","what"))},"e"),a("span",{class:"n",onClick:e[3]||(e[3]=e=>s.trigger("n","where"))},"n"),a("span",{class:"e2",onClick:e[4]||(e[4]=e=>s.trigger("e2","when"))},"e"),a("span",{class:"i",onClick:e[5]||(e[5]=e=>s.trigger("i","why"))},"i"),a("small",{onClick:e[6]||(e[6]=e=>s.trigger("web","/"))},[d,p,w])],2)])}));g.render=b,g.__scopeId="data-v-ca4cb224";const f=i(l(g));f.mount("#senei"),window.senei=f,new FontFaceObserver("StencilRough").load().then((function(){document.body.classList.add("has-stencil")}));
+import {
+	withScopeId as e,
+	pushScopeId as t,
+	popScopeId as n,
+	openBlock as s,
+	createBlock as a,
+	defineComponent as o,
+	ref as c,
+	computed as r,
+	createVNode as i,
+	createApp as d,
+} from "./web_modules/vue/vue.runtime.esm-browser.prod.js";
+import { createRouter as cr, createWebHistory as cwh } from "./web_modules/vue/vue-router.esm.js";
+
+var l = {};
+const p = e("data-v-51e68c33");
+t("data-v-51e68c33");
+const h = { class: "hidden" };
+n();
+const u = p(function (e, t) {
+	return s(), a("div", h, " home ");
+});
+(l.render = u), (l.__scopeId = "data-v-51e68c33");
+var m = o(l),
+	v = {};
+const w = e("data-v-f6d46782");
+t("data-v-f6d46782");
+const f = { class: "hidden" };
+n();
+const g = w(function (e, t) {
+	return s(), a("div", f, " contact.vue ");
+});
+(v.render = g), (v.__scopeId = "data-v-f6d46782");
+var b = o(v);
+const _ = cwh("/");
+let C = cr({
+	history: _,
+	routes: [
+		{ path: "/", component: m },
+		{ path: "/who", component: b },
+		{ path: "/what", component: b },
+		{ path: "/where", component: b },
+		{ path: "/when", component: b },
+		{ path: "/why", component: b },
+	],
+});
+var k = {
+	setup() {
+		const e = c("start"),
+			t = r(() => "flex-auto text-center name title-transform--" + e.value);
+		return {
+			transformId: e,
+			transformClass: t,
+			trigger: function (t, n) {
+				(e.value = t), C.push(n);
+			},
+		};
+	},
+};
+const y = e("data-v-e8ba3f3c");
+t("data-v-e8ba3f3c");
+const x = { class: "flex mx-auto mt-5 mb-5 logo" },
+	I = i("span", { class: "_w" }, "w", -1),
+	F = i("span", { class: "_e" }, "e", -1),
+	R = i("span", { class: "_b" }, "b", -1);
+n();
+const j = y(function (e, t) {
+	return (
+		s(),
+		a("section", x, [
+			i(
+				"h1",
+				{ class: e.transformClass },
+				[
+					i(
+						"span",
+						{
+							class: "s",
+							onClick: t[1] || (t[1] = (t) => e.trigger("s", "who")),
+						},
+						"s"
+					),
+					i(
+						"span",
+						{
+							class: "e1",
+							onClick: t[2] || (t[2] = (t) => e.trigger("e1", "what")),
+						},
+						"e"
+					),
+					i(
+						"span",
+						{
+							class: "n",
+							onClick: t[3] || (t[3] = (t) => e.trigger("n", "where")),
+						},
+						"n"
+					),
+					i(
+						"span",
+						{
+							class: "e2",
+							onClick: t[4] || (t[4] = (t) => e.trigger("e2", "when")),
+						},
+						"e"
+					),
+					i(
+						"span",
+						{
+							class: "i",
+							onClick: t[5] || (t[5] = (t) => e.trigger("i", "why")),
+						},
+						"i"
+					),
+					i(
+						"small",
+						{ onClick: t[6] || (t[6] = (t) => e.trigger("web", "/")) },
+						[I, F, R]
+					),
+				],
+				2
+			),
+		])
+	);
+});
+(k.render = j), (k.__scopeId = "data-v-e8ba3f3c");
+const H = d(o(k));
+H.use(C),
+	H.mount("#senei"),
+	(window.senei = H),
+	(window.senei_router = C),
+	new FontFaceObserver("StencilRough").load().then(function () {
+		document.body.classList.add("has-stencil");
+	});
